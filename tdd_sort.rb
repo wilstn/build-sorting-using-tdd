a52e03fcd420c3c351281b1f48f6affae056c703
icebear <wilsontan.2088@gmail.com>
2017-04-26 01:06:55 +0800

    Write test and pass for test case 2

--- tdd_sort.rb
+++ tdd_sort.rb
@@ -1,14 +1,19 @@
 require 'test/unit'
 
 def sort(arr)
-  []
+  if arr == []
+    []
+  else
+    [1]
+  end
 end
 
 
-class TestSimpleNumber < Test::Unit::TestCase
+class TestSortArray < Test::Unit::TestCase
 
-  def test_simple
+  def test_sort
     assert_equal([], sort([]))
+    assert_equal([1], sort([1]))
   end
-  
+
 end
